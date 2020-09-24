6eaa207aa004d970ad23dfe2f744ea9cbf4686f1
thanakorn <thanakorn.panyapiang@outlook.com>
2020-09-24 11:52:46 +0100

    VOCWriter

---
+++ voc/annotation.py
@@ -0,0 +1,27 @@
+class Annotation:
+    def __init__(self, folder, filename, path, database, size, segmented=0):
+        self.folder = folder
+        self.filename = filename
+        self.path = path
+        self.database = database
+        self.size = size
+        self.segmented = segmented
+        self.objects = []
+        
+    def add_object(self, obj):
+        self.objects.append(obj)
+
+class Object:
+    def __init__(self, name, xy_min, xy_max, pose='Unspecified', truncated=0, difficult=0):
+        self.name = name
+        self.pose = pose
+        self.truncated = truncated
+        self.xmin, self.ymin = xy_min
+        self.xmax, self.ymax = xy_max
+        self.difficult = difficult
+        
+class Size:
+    def __init__(self, width, height, depth):
+        self.width = width
+        self.height = height
+        self.depth = depth
---
+++ voc/writer.py
@@ -0,0 +1,32 @@
+import xml.etree.ElementTree as et
+from copy import deepcopy
+
+class VOCWriter:
+    def __init__(self):
+        self.annotation_template = et.parse('./templates/annotation.xml')
+        self.object_template = et.parse('./templates/object.xml')       
+    
+    def write(self, annotation, file):
+        output = deepcopy(self.annotation_template).getroot()
+        output.find('folder').text = annotation.folder
+        output.find('filename').text = annotation.filename
+        output.find('path').text = annotation.path
+        output.find('source').find('database').text = annotation.database
+        output.find('size').find('width').text = str(annotation.size.width)
+        output.find('size').find('height').text = str(annotation.size.height)
+        output.find('size').find('depth').text = str(annotation.size.depth)
+        
+        for obj in annotation.objects:
+            obj_output = deepcopy(self.object_template).getroot()
+            obj_output.find('name').text = obj.name
+            obj_output.find('pose').text = obj.pose
+            obj_output.find('truncated').text = str(obj.truncated)
+            obj_output.find('difficult').text = str(obj.difficult)
+            obj_output.find('bndbox').find('xmin').text = str(obj.xmin)
+            obj_output.find('bndbox').find('ymin').text = str(obj.ymin)
+            obj_output.find('bndbox').find('xmax').text = str(obj.xmax)
+            obj_output.find('bndbox').find('ymax').text = str(obj.ymax)
+            output.append(obj_output)
+        
+        tree = et.ElementTree(output)
+        tree.write(file)
