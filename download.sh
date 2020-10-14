8e50a24db327bb60a1d5052754bb76447ac6f89d
Thanakorn Panyapiang <thanakorn.panyapiang@outlook.com>
2020-10-14 19:58:12 +0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,13 +1,15 @@
+sudo apt-get install p7zip-full
+
 mkdir data
 cd data
 
 echo "Downloading Annotations"
-wget https://zenodo.org/record/3723295/files/annotations.csv?download=1 -O annotations.csv
+wget https://zenodo.org/record/3723295/files/annotations.csv
 
-for i in {0..1}
+for i in 1
 do
-  echo "Downloading subset$i"
-  wget "https://zenodo.org/record/3723295/files/subset$i.zip?download=1 -O subset$i.zip"
-  unzip "subset$i.zip"
-  rm "subset$i.zip"
+  echo 'Downloading subset$i'
+  wget https://zenodo.org/record/3723295/files/subset$i.zip
+  7z subset$i.zip
+  # rm subset$i.zip
 done
