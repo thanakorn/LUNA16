2515263eaff988ccfab7dc37e9837b7b1d6f0708
Thanakorn Panyapiang <thanakorn.panyapiang@outlook.com>
2020-10-14 18:21:51 +0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -4,7 +4,10 @@ cd data
 echo "Downloading Annotations"
 wget https://zenodo.org/record/3723295/files/annotations.csv?download=1 -O annotations.csv
 
-echo "Downloading subset0"
-wget https://zenodo.org/record/3723295/files/subset0.zip?download=1 -O subset0.zip
-unzip subset0.zip
-rm subset0.zip
+for i in {0..1}
+do
+  echo "Downloading subset$i"
+  wget "https://zenodo.org/record/3723295/files/subset$i.zip?download=1 -O subset$i.zip"
+  unzip "subset$i.zip"
+  rm "subset$i.zip"
+done
