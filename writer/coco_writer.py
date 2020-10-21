30be6d84390923f969bbc687d1342c2af4806ae4
thanakorn <thanakorn.panyapiang@outlook.com>
2020-10-21 17:04:20 +0700

    Fix COCOWriter bug

--- writer/coco_writer.py
+++ writer/coco_writer.py
@@ -8,10 +8,10 @@ class COCOWriter:
         file = open(file, 'w+')
         for obj in annotation.objects:
             class_id = classes.index(obj.name)
-            box_width = float(obj.xmax - obj.xmin) / annotation.width
-            box_height = float(obj.ymax - obj.ymin) / annotation.height
-            x_center = (obj.xmin + (box_width / 2.)) / annotation.width
-            y_center = (obj.ymin + (box_height / 2.)) / annotation.height
-            file.write(f'{class_id} {x_center} {y_center} {box_width} {box_height}\n')
+            box_width = float(obj.xmax - obj.xmin)
+            box_height = float(obj.ymax - obj.ymin)
+            x_center = (obj.xmin + (box_width / 2.))
+            y_center = (obj.ymin + (box_height / 2.))
+            file.write(f'{class_id} {x_center / annotation.width } {y_center / annotation.height} {box_width / annotation.width } {box_height / annotation.height}\n')
             
             
